5f500642a3cdaa1f60c809425d7b3b8659aa0e1d
Pablo Gramajo <pdp1985gramajo@gmail.com>
2017-04-12 16:05:34 -0300

    save

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -2,9 +2,11 @@ using DOTNET_APPS.Models;
 using JsonApiDotNetCore.Controllers;
 using JsonApiDotNetCore.Data;
 using JsonApiDotNetCore.Services;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 namespace DOTNET_APPS.Controllers
 {
+    [Route("api/[controller]")]
     public class PeopleController : JsonApiController<Person>
     {
          public PeopleController(
